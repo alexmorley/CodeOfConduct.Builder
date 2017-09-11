bf50995338743d8d1af828d654a496652707daec
Alexander Morley <alexander.morley@univ.ox.ac.uk>
2017-09-11 20:17:39 +0100

    added first selection events

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,28 @@
 function onLoad() {
-	//$('#code-generator').hide(); 
-	$('#coc-base').on('click', function() {
-		var selectedBase = $('#coc-base option:selected').val();
-		var json_obj;
-		$.ajax({
-		  url: "resources/ContributorCovenant.json",
-		  dataType: "json",
-		  async: false,
-		  success: function(response) {
-		  	json_obj = response;
-		  }
-		  })        
+    //$('#rightFrame').hide(); 
+    $('#coc-base').on('change', function() {
+        var selectedBase = $('#coc-base option:selected').val();
+        console.log(selectedBase);
+        var json_obj;
+        $.ajax({
+            url: "resources/"+selectedBase+".json",
+            dataType: "json",
+            async: false,
+            success: function(response) {
+                json_obj = response;
+            }
+        })        
+        console.log("Read attempted");
+        $('#sub-title').text(json_obj["Title"]);
 
-		$('#base-text').text(json_obj["Title"]);
-	});
+        // $('#encouragedBehaviours'.text(
+        
+        $("#linkList").empty();
+        $.each(json_obj.UnacceptableBehaviour.list, function( i, point ) {
+            var $li = $("<li><a>"+point+"</a></li>");      
+            $("#linkList").append($li);            
+        });
+    });
 }
-
+console.log("yep");
 $(document).ready(onLoad);
-
