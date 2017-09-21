f42b530361895ff559b50ab1c08bfc441b26fcb9
Alexander Morley <alexander.morley@univ.ox.ac.uk>
2017-09-21 18:09:36 +0100

    updated download for hashing

--- download.php
+++ download.php
@@ -1,6 +1,7 @@
 <?php
-header("Content-disposition: attachment; filename=your_license.".$_POST["ext"]);
+header("Content-disposition: attachment; filename=your_license.".$_POST["id"].".".$_POST["ext"]);
 header("Content-type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
 $ext = $_POST["ext"];
-readfile("license_tmp/your_licence.$ext");
+$id = $_POST["id"];
+readfile("license_tmp/your_licence.$id.$ext");
 ?>
