23d4dd32edad83d901e8228aa12da16387c31fce
DovidM <landyx47@gmail.com>
2017-10-01 16:14:53 -0400

    Added tinymce editor

--- app/builder.php
+++ app/builder.php
@@ -9,13 +9,16 @@ include("head.php");
 include("header.php");
 ?>
 
+<script defer src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+<script defer src="../assets/js/builder.js"></script>
+
 <div class="container">
     <div class="leftFrame" id="leftFrame" scrolling="no">
         <div class="container" id="code-generator">
             <h3>Getting Started</h3>
             <div id="your-info">
                 <label>What is the name of your event?</label>
-                <input type="text" name="event-name" id="event-name"> 
+                <input type="text" name="event-name" id="event-name">
             </div>
             <div id="cocPreset">
                 <label>Choose a Code of Conduct to start remixing?</label>
@@ -31,8 +34,9 @@ include("header.php");
     </div>
 
     <div class="rightFrame" id = "noedit">
-        <div id="rightFrame" contenteditable="true">
-            <h2 id = your-title>Preview - Your Title Goes Here </h2> 
+
+        <textarea id="rightFrame">
+            <h2 id = your-title>Preview - Your Title Goes Here </h2>
             <h4 id = "sub-title-pre" style="display: inline; font-weight:bold"> Based on:</h4> <h4 id = "sub-title" style="display: inline-block"> </h4>
             <h3>Encouraged Behaviour</h3>
             <ul id="goodlist"></ul>
@@ -41,7 +45,8 @@ include("header.php");
             <ul id="badlist"></ul>
             <h3>How can I report a violation of the Code of Conduct?</h3>
             <ul id="reporting"></ul>
-        </div>
+        </textarea>
+
         <form action="download.php" style="display: inline-block; float:left; padding-bottom:75" method="post">
             <input type="submit" id="download" value="Download As:">
             <select id="ext_selector" style="display: inline-block; width: 50%" name="ext">
@@ -50,7 +55,7 @@ include("header.php");
                 <option value="odt">.odt (Open Office)</option>
                 <option value="html">.html (Web Page)</option>
             </select>
-            <input type="hidden" id="id" name="id" value="0000000"> 
+            <input type="hidden" id="id" name="id" value="0000000">
         </form>
     </div>
 </div>
