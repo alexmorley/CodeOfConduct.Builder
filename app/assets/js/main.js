3edcc74d43178b9ca62f0aee985ff0eb236d0cb3
DovidM <landyx47@gmail.com>
2017-10-01 16:51:44 -0400

    Redid js to account for tinymce

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,56 +1,51 @@
 function onLoad() {
-    $('#coc-base').on('change', function() {
+    $('#coc-base').on('change', async function() {
         var selectedBase = $('#coc-base option:selected').val();
         console.log(selectedBase);
-        var json_obj;
-        $.ajax({
-            url: "resources/"+selectedBase+".json",
-            dataType: "json",
-            async: false,
-            success: function(response) {
-                json_obj = response;
-            }
-        })        
+
+        const response = await fetch("resources/"+selectedBase+".json");
+        const json_obj = await response.json();
+
         console.log("Read attempted");
-        $('#sub-title').text(json_obj["title"]);
+
+        const editor = tinymce.editors[0];
+        const $conductDocument = $(editor.getBody());
+
+        $conductDocument.find('#sub-title').text(json_obj["title"]);
 
 
-        $("#badlist").empty();
+        $conductDocument.find("#badlist").empty();
         $.each(json_obj.unacceptable_behaviour.list, function( i, point ) {
-            var $li = $("<li><a>"+point+"</a></li>");      
-            $("#badlist").append($li);            
+            var $li = $("<li><a>"+point+"</a></li>");
+            $conductDocument.find("#badlist").append($li);
         });
 
-        $("#goodlist").empty();
+        $conductDocument.find("#goodlist").empty();
         $.each(json_obj.encouraged_behaviour.list, function( i, point ) {
-            var $li = $("<li><a>"+point+"</a></li>");      
-            $("#goodlist").append($li);            
+            var $li = $("<li><a>"+point+"</a></li>");
+            $conductDocument.find("#goodlist").append($li);
         });
 
-        $("#reporting").empty();
+        $conductDocument.find("#reporting").empty();
         $.each(json_obj.reporting.howto, function( i, point ) {
             var $li = $("<li><a>"+point+"</a></li>");
-            $("#reporting").append($li);
-        }); 
+            $conductDocument.find("#reporting").append($li);
+        });
     });
 
     $('#event-name').on('input', function() {
         $('#your-title').text(this.value + '\'s Code of Conduct');
     });
 
-    $("#rightFrame").on('input', function() {
-        $("#ext_selector").val("none");
-    }); 
-
     $("#ext_selector").on('change', function () {
         var text = $("#rightFrame").html();
         var ext_info = $("#ext_selector option:selected").val();
 	if (ext_info != "none") {
-            var id_info = CRC32.str(text); 
+            var id_info = CRC32.str(text);
             $("#id").val(id_info);
             var url = "save.php";
             console.log(ext_info);
-            $.post(url, 
+            $.post(url,
                     {ext: ext_info,
                         rightFrame: text,
                         id: id_info
@@ -58,7 +53,7 @@ function onLoad() {
                         console.log(text.length + " characters sent. Hash: " + id_info + "Ext: " + ext_info);
                     });
         }
-    }); 
+    });
 }
 console.log("yep");
 $(document).ready(onLoad);
