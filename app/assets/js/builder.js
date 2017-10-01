9610db5ab6b9852fb8da6d2d2d059afe1853f515
DovidM <landyx47@gmail.com>
2017-10-01 16:52:00 -0400

    Made settings simpler

--- app/assets/js/builder.js
+++ app/assets/js/builder.js
@@ -1,9 +1,10 @@
 tinymce.init({
 
     selector: `#rightFrame`,
-    plugins: 'wordcount table fullscreen autolink autoresize help image paste code',
-    menubar: 'view help edit tools',
-    valid_elements: 'abbr[!title],h1,h2,h3,h4,h5,h6,img[!src|alt],p,a[!href],table,' +
-    'td,tr,th,tbody,thead,tfoot,strong,em,u,ul,ol,li,q,blockquote,pre,br',
-    paste_data_images: true
+    plugins: "table fullscreen autolink autoresize help image paste code",
+    menubar: "view help edit tools",
+    init_instance_callback: editor => {
+
+        editor.on("input", () => $("#ext_selector").val("none"));
+    }
 });
