767922a315c4cea41aaa7aac9fe3dafd9da3d8d0
dandetiger <amylaun@gmail.com>
2017-10-03 21:18:57 -0700

    Merge Grid CSS into Main
    * Merged dual stylesheets, just needs to be one

--- app/head.php
+++ app/head.php
@@ -4,7 +4,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
 <link rel="stylesheet" href="./node_modules/milligram/dist/milligram.min.css">
 <link rel="stylesheet" href="https://milligram.github.io/styles/main.css">
-<link rel="stylesheet" href="assets/css/grid.css">
 <link rel="stylesheet" href="assets/css/main.css">
 <link rel="icon" href="assets/images/signpost.png">
 <title>Code of Conduct Builder</title>
