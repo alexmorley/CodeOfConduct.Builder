d5fe3e687392ddb9b2f13b929104ccf6b61cb819
DovidM <landyx47@gmail.com>
2017-10-11 14:11:58 -0400

    Fixed error where file to download didn't exist

--- app/builder.php
+++ app/builder.php
@@ -37,18 +37,21 @@ include("header.php");
     </div>
 
     <div class="col rightFrame" id = "noedit">
-        <textarea id="rightFrame">
-        <!--div id="rightFrame" contenteditable="true">-->
-            <h2 id = your-title>Preview - Your Title Goes Here </h2>
-            <h4 id = "sub-title-pre" style="display: inline; font-weight:bold"> Based on:</h4> <h4 id = "sub-title" style="display: inline-block"> </h4>
-            <h3>Encouraged Behaviour</h3>
-            <ul id="goodlist"></ul>
-            <h3>Unacceptable Behaviour</h3>
-            <ul id="badlist"></ul>
-            <h3>How can I report a violation of the Code of Conduct?</h3>
-            <ul id="reporting"></ul>
-        </textarea>
+
         <form action="download.php" style="display: inline-block; float:left; padding-bottom:75" method="post" id="downloadForm">
+
+            <textarea name="content" id="rightFrame">
+            <!--div id="rightFrame" contenteditable="true">-->
+                <h2 id = your-title>Preview - Your Title Goes Here </h2>
+                <h4 id = "sub-title-pre" style="display: inline; font-weight:bold"> Based on:</h4> <h4 id = "sub-title" style="display: inline-block"> </h4>
+                <h3>Encouraged Behaviour</h3>
+                <ul id="goodlist"></ul>
+                <h3>Unacceptable Behaviour</h3>
+                <ul id="badlist"></ul>
+                <h3>How can I report a violation of the Code of Conduct?</h3>
+                <ul id="reporting"></ul>
+            </textarea>
+
             <input type="submit" id="download" value="Download As:">
             <select id="ext_selector" style="display: inline-block; width: 50%" name="ext">
                 <option value="none" selected="selected">Choose a file extension</option>
