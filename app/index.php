2c262a51c62ea4d225dfeb1568cf841f1996f310
univ3573 <alexander.morley@univ.ox.ac.uk>
2017-10-28 11:58:13 +0100

    reformatted logos

--- app/index.php
+++ app/index.php
@@ -5,9 +5,14 @@ include("head.php");
 </head>
 
 <body class="main">
-<?php
-include("header.php");
-?>
+
+<span class="banner"> This website is still in development (pre-beta) at the moment. Please send feedback to <a href="mailto:alex0morley@gmail.com?Subject=Code%20of%20Conducts">alex0morley@gmail.com</a> or @alexmorley on twitter. Thanks!</span>
+
+<div id="title" class="container" text-align="center"> 
+    <a href="/" style="color:inherit"><img src="assets/images/logo.png" alt="Code of Conduct Builder Logo" style="height: 25%; margin-left:auto; margin-right:auto; display: block; padding-top: 5%"></a>
+</div>
+
+<main>
 
 <div class="content-wrapper--large landing">
 <a class="button" href=./builder.php>Build a Code of Conduct</a><br>
