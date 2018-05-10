ad8802a6b7365b07d81dafe734abcb75e31ac8e1
alexmorley <alexander.morley@univ.ox.ac.uk>
2018-05-10 17:01:44 +0100

    electron app almost working

--- app/cocbuilder/routes/builder.js
+++ app/cocbuilder/routes/builder.js
@@ -2,11 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/builder', function(req, res, next) {
+router.get('builder', function(req, res, next) {
   res.render('builder', { title: 'Builder' });
 });
 
