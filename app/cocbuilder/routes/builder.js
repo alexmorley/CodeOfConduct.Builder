a4295934535f4ca0b0cd1050fbaedb79a72dd302
alexmorley <alexander.morley@univ.ox.ac.uk>
2018-05-11 13:10:05 +0100

    copy to app2

--- app/cocbuilder/routes/builder.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('builder', function(req, res, next) {
-  res.render('builder', { title: 'Builder' });
-});
-
-module.exports = router;
