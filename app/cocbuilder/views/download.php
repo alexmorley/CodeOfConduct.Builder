23d79e337b6a2201591974627dc0732689dccb5c
alexmorley <alexander.morley@univ.ox.ac.uk>
2018-05-11 13:26:58 +0100

    re-shuffle directories

--- app/cocbuilder/views/download.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-disposition: attachment; filename=your_license.".$_POST["id"].".".$_POST["ext"]);
-header("Content-type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-$ext = $_POST["ext"];
-$id = $_POST["id"];
-readfile("license_tmp/your_licence.$id.$ext");
-?>
