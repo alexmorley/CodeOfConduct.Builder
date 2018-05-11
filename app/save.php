ecf5e9fa680d0dafc25cacb3145b8be799efd1ad
Pankaj Kumar Gautam <pnkjgautam05@gmail.com>
2018-05-11 20:43:20 +0530

    sanitising user input (#51)

--- app/save.php
+++ app/save.php
@@ -5,11 +5,9 @@ if ( !file_exists($dir) ) {
 	$oldmask = umask(0);
 	mkdir ($dir, 0775); 
 }
-
-$content = $_POST["rightFrame"];
-$id = $_POST["id"];
-$ext = $_POST["ext"];
-
+$content = filter_var($_POST["rightFrame"], FILTER_SANITIZE_STRING);
+$id = filter_var($_POST["id"], FILTER_SANITIZE_STRING);
+$ext = filter_var($_POST["ext"], FILTER_SANITIZE_STRING);
 $dump_name = "$dir/post.$id.dump";
 file_put_contents($dump_name, $content);
 chmod($dump_name, 0664);
